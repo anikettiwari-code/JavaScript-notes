9abddb50ddd7cfb7f04913520d9395d7b792296e
anikettiwari02 <tiwarianiket0203@gmail.com>
2026-06-28 19:06:48 +0530

    Number and maths

--- 6-Number&maths.js
+++ 6-Number&maths.js
@@ -20,17 +20,10 @@
 // console.log(Math.ceil(4.2));
 // console.log(Math.floor(4.9)); 
 
-console.log(Math.random()); // Returns a pseudorandom number between 0 and 1.
-console.log((Math.random()*10)+ 1); 
-console.log(Math.floor(Math.random()*10) + 1);
-
-const min = 10
-const max = 20
-console.log(Math.floor(Math.random() * (max - min + 1)) + min);
-
-
-
-
-
-
-
+// console.log(Math.random()); // Returns a pseudorandom number between 0 and 1.
+// console.log((Math.random()*10)+ 1); 
+// console.log(Math.floor(Math.random()*10) + 1);
+// 
+// const min = 10
+// const max = 20
+// console.log(Math.floor(Math.random() * (max - min + 1)) + min);
