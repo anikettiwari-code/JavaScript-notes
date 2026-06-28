0eadd970e8c5bc43edc404c1b6297ba2d6f7b775
anikettiwari02 <tiwarianiket0203@gmail.com>
2026-06-28 19:08:32 +0530

    Dates js

---
+++ 7-Date.js
@@ -0,0 +1,17 @@
+//Dates
+
+let myDate = new Date()
+//console.log(myDate.toString()); // Sun Apr 26 2026 13:12:58 GMT+0530 (India Standard Time)
+//console.log(myDate.toDateString()); // Sun Apr 26 2026 
+//console.log(myDate.toLocaleString()); // 24/4/2026, 1:16:20
+//console.log(myDate.toLocaleDateString()); // 26/4/2026
+//console.log(myDate.toLocaleTimeString()); // 1:16:20
+//console.log(typeof myDate); // object
+
+let myCreatedDate = new Date(2026, 0, 23, 5, 35)
+console.log(myCreatedDate.toDateString()); //Fri Jan 23 2026
+console.log(myCreatedDate.toLocaleDateString()); //23/1/2026
+console.log(myCreatedDate.toLocaleString()); //23/1/2026, 5:35:00 am
+
+let myCreateDate = new Date("2026-01-14")
+console.log(myCreateDate.toLocaleString());
