d6d7158d05376d0521787bdf7000c2e86dbeaf5a
anikettiwari02 <tiwarianiket0203@gmail.com>
2026-06-30 17:56:22 +0530

    date imp types

--- 7-Date.js
+++ 7-Date.js
@@ -1,6 +1,6 @@
 //Dates
 
-let myDate = new Date()
+//let myDate = new Date()
 //console.log(myDate.toString()); // Sun Apr 26 2026 13:12:58 GMT+0530 (India Standard Time)
 //console.log(myDate.toDateString()); // Sun Apr 26 2026 
 //console.log(myDate.toLocaleString()); // 24/4/2026, 1:16:20
@@ -8,10 +8,12 @@ let myDate = new Date()
 //console.log(myDate.toLocaleTimeString()); // 1:16:20
 //console.log(typeof myDate); // object
 
-let myCreatedDate = new Date(2026, 0, 23, 5, 35)
-console.log(myCreatedDate.toDateString()); //Fri Jan 23 2026
-console.log(myCreatedDate.toLocaleDateString()); //23/1/2026
-console.log(myCreatedDate.toLocaleString()); //23/1/2026, 5:35:00 am
+// let myCreatedDate = new Date(2026, 0, 23, 5, 35)
+// console.log(myCreatedDate.toDateString()); //Fri Jan 23 2026
+// console.log(myCreatedDate.toLocaleDateString()); //23/1/2026
+// console.log(myCreatedDate.toLocaleString()); //23/1/2026, 5:35:00 am
 
-let myCreateDate = new Date("2026-01-14")
+//let myCreateDate = new Date("2026-01-14")
+let myCreateDate = new Date("01-14-2023") //india use
 console.log(myCreateDate.toLocaleString());
+
