14494cf4bdb2743d41b2065f838e88c50a96bf03
Aniket Tiwari <tiwarianiket8228@gmail.com>
2026-07-04 11:56:42 +0000

    puch and concat

---
+++ 8.1-Array.js
@@ -0,0 +1,9 @@
+const marval_heros = [ "iraonman", "thor","spiderman"]
+const dc_heros = [ "suparman", "flash","batman"]
+
+//marval_heros.push(dc_heros) // [ 'iraonman', 'thor', 'spiderman', [ 'suparman', 'flash', 'batman' ] ]
+// console.log(marval_heros);
+
+// const all_heros = marval_heros.concat(dc_heros) // [ 'iraonman', 'thor', 'spiderman', 'suparman', 'flash', 'batman' ]
+// console.log(all_heros);
+
