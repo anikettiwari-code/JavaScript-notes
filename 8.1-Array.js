bfecedc2c54cd9224e4bdf89cd9db69a4a7c6e6b
Aniket Tiwari <tiwarianiket8228@gmail.com>
2026-07-04 11:58:11 +0000

    Spread operator

--- 8.1-Array.js
+++ 8.1-Array.js
@@ -7,3 +7,7 @@ const dc_heros = [ "suparman", "flash","batman"]
 // const all_heros = marval_heros.concat(dc_heros) // [ 'iraonman', 'thor', 'spiderman', 'suparman', 'flash', 'batman' ]
 // console.log(all_heros);
 
+// Spread operator
+
+const all_newheros = [...marval_heros, ...dc_heros] // [ 'iraonman', 'thor', 'spiderman', 'suparman', 'flash', 'batman' ]
+console.log(all_newheros);
