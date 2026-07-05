357cb4a90c94583456a8e9f4ecc45609d9395a40
Aniket Tiwari <tiwarianiket8228@gmail.com>
2026-07-05 14:41:15 +0000

    symbol object

--- 9-objects.js
+++ 9-objects.js
@@ -1,8 +1,13 @@
 // object literals
+// symbal 
+
+const mySym = Symbol("key1")
+
 
 const JsUser ={
     name : "Aniket",
     "full name": "Aniket tiwari",
+    [mySym]:"myKey1",
     age: 20,
     location: "Mumbai",
     email: "aniket@emi.com",
@@ -13,5 +18,7 @@ const JsUser ={
 console.log(JsUser.email) // aniket@emi.com
 console.log(JsUser["email"]) // aniket@emi.com
 //console.log(JsUser.full name);// but don't use (.) in in object it's so error
+console.log(JsUser["full name"]); // Aniket tiwari
+console.log(typeof JsUser[mySym]); // using [] to show symbal
 
-console.log(JsUser["full name"]);
+
