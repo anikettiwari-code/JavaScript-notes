9cb0b927bfff1d7c025ec70af5ebbbe330286c99
Aniket Tiwari <tiwarianiket8228@gmail.com>
2026-07-05 15:23:06 +0000

    part 1 done

--- 9-objects.js
+++ 9-objects.js
@@ -15,13 +15,23 @@ const JsUser ={
     lastLoginDays: ["monday" , "Saturday" ]
 }
 
-console.log(JsUser.email) // aniket@emi.com
-console.log(JsUser["email"]) // aniket@emi.com
-//console.log(JsUser.full name);// but don't use (.) in in object it's so error
-console.log(JsUser["full name"]); // Aniket tiwari
-console.log(JsUser[mySym]); // using [] to show symbal // string
+// console.log(JsUser.email) // aniket@emi.com
+// console.log(JsUser["email"]) // aniket@emi.com
+// //console.log(JsUser.full name);// but don't use (.) in in object it's so error
+// console.log(JsUser["full name"]); // Aniket tiwari
+// console.log(JsUser[mySym]); // using [] to show symbal // string
 
-JsUser.email = "aniket@goo.com" // change the email to aniket@goo.com
-Object.freeze(JsUser) // use to freeze the object
+// JsUser.email = "aniket@goo.com" // change the email to aniket@goo.com
+// Object.freeze(JsUser) // use to freeze the object
+
+JsUser.greeting = function(){
+    console.log("Hello js user");
+}
+
+JsUser.greetingtwo = function(){
+    console.log('Hello js user,${this.name}');
+}
 
 
+console.log(JsUser.greeting());
+console.log(JsUser.greetingtwo());
