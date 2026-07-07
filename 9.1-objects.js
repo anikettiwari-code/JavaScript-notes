40799b5e0b0c3eef012ac9c6e64ab86bbe8d7835
Aniket Tiwari <tiwarianiket8228@gmail.com>
2026-07-07 14:06:54 +0000

    object inthe object

--- 9.1-objects.js
+++ 9.1-objects.js
@@ -1,3 +1,27 @@
 // siggleton 
 
-const 
+//const tinderUser = new Object() // siggleton obj
+const tinderUser ={} // non siggleton obj the output are same {}
+
+tinderUser.id = "123add"
+tinderUser.name = "rama"
+tinderUser.isLoggedIn = false
+
+//console.log(tinderUser);
+
+const regualarUser ={
+    email:"some@gmaiil.com",
+    fullname:{
+        userfullname:{
+            firstname: "ramma",
+            lastname: "ji"
+        }
+    }
+}
+
+console.log(regualarUser.fullname.userfullname.firstname);
+
+const obj1 = {1: "a" , 2: "b"}
+const obj2 = {3: "a" , 4: "b"}
+
+const obj3 = { }
